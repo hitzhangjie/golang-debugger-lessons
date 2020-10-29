8acebd8925f42e646a93d07a1af0d220eabc9cf9
hitzhangjie <hit.zhangjie@gmail.com>
2020-10-30 00:22:53 +0800

    添加docker选项说明

--- docker.sh
+++ docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -e
 
-docker build -t debugger.env . && \
-docker run -it \
--v /Users/zhangjie/debugger101/golang-debugger-lessons:/root/debugger101 \
---name debugger.env --cap-add SYS_PTRACE --security-opt seccomp=unconfined  --rm debugger.env \
+# rebuild the image
+docker build -t debugger.env .
+
+# ptrace need priviledges enabled, like `--cap-add SYS_PTRACE --security-opt seccomp=unconfined`
+docker run -it 															   \
+-v /Users/zhangjie/debugger101/golang-debugger-lessons:/root/debugger101   \
+--name debugger.env --cap-add SYS_PTRACE --security-opt seccomp=unconfined \
+--rm debugger.env 														   \
 /bin/bash
 
