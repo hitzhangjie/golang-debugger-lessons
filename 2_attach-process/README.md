304e1b65ea492d96a331d52114ff042d301db822
hitzhangjie <hit.zhangjie@gmail.com>
2020-10-30 00:20:11 +0800

    支持attach 到指定进程

---
+++ 2_attach-process/README.md
@@ -0,0 +1,3 @@
+课程目标：attach到一个运行进程
+
+示例：`go run main.go attach <pid>`
--- 2_attach-process/main.go
+++ 2_attach-process/main.go
@@ -1,1 +1,98 @@
-package __attach_process
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"syscall"
+)
+
+const (
+	usage = "Usage: go run main.go exec <path/to/prog>"
+
+	cmdExec   = "exec"
+	cmdAttach = "attach"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "%s\n\n", usage)
+		os.Exit(1)
+	}
+	cmd := os.Args[1]
+
+	switch cmd {
+	case cmdExec:
+		prog := os.Args[2]
+
+		// check whether prog exists
+		fin, err := os.Lstat(prog)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "lstat error: %v\n\n", err)
+			os.Exit(1)
+		}
+
+		if fin.IsDir() {
+			fmt.Fprintf(os.Stderr, "%s is directory\n\n", prog)
+			os.Exit(1)
+		}
+
+		// check whether prog executable
+		if fin.Mode()&0111 == 0 {
+			fmt.Fprintf(os.Stderr, "%s not executable\n\n", prog)
+			os.Exit(1)
+		}
+
+		// run prog
+		progCmd := exec.Command(prog)
+		buf, err := progCmd.CombinedOutput()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s exec error: %v, \n\n%s\n\n", err, string(buf))
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stdout, "%s\n", string(buf))
+
+	case cmdAttach:
+		pid, err := strconv.ParseInt(os.Args[2], 10, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s invalid pid\n\n", os.Args[2])
+			os.Exit(1)
+		}
+
+		if !checkPid(int(pid)) {
+			fmt.Fprintf(os.Stderr, "process %d not existed\n\n", pid)
+			os.Exit(1)
+		}
+
+		err = syscall.PtraceAttach(int(pid))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "process %d attach error: %v\n\n", pid, err)
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stdout, "process %d attach succ\n\n", pid)
+
+	default:
+		fmt.Fprintf(os.Stderr, "%s unknown cmd\n\n", cmd)
+		os.Exit(1)
+	}
+
+}
+
+// checkPid check whether pid is valid process's id
+//
+// On Unix systems, os.FindProcess always succeeds and returns a Process for
+// the given pid, regardless of whether the process exists.
+func checkPid(pid int) bool {
+	out, err := exec.Command("kill", "-s", "0", strconv.Itoa(pid)).CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
+
+	// output error message, means pid is invalid
+	if string(out) != "" {
+		return false
+	}
+
+	return true
+}
