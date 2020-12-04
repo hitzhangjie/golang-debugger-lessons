bbc67e9d4b6de9a474f8c19a041dcea5d0ff24c3
hitzhangjie <hit.zhangjie@gmail.com>
2020-12-04 21:37:01 +0800

    6-gosym：适当优化下代码逻辑

--- 6_gosym/main.go
+++ 6_gosym/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/davecgh/go-spew/spew"
 	"golang.org/x/arch/x86/x86asm"
@@ -18,130 +19,68 @@ func main() {
 	}
 	prog := os.Args[1]
 
-	// open elf
+	//--------------------------------------------------------------------
+	// open elf file
+
+	fmt.Println("processing elf file: ", prog)
+
 	file, err := elf.Open(prog)
 	if err != nil {
 		panic(err)
 	}
 	//spew.Dump(file)
+	println()
 
-	// prog headers
-	//tw := tabwriter.NewWriter(os.Stdout, 0, 4, 3, ' ', 0)
-	//fmt.Fprintf(tw, "No.\tType\tFlags\tVAddr\tMemSize\n")
-	//for idx, p := range file.Progs {
-	//	fmt.Fprintf(tw, "%d\t%v\t%v\t%#x\t%d\n", idx, p.Type, p.Flags, p.Vaddr, p.Memsz)
-	//}
-	//tw.Flush()
-	//println()
+	//---------------------------------------------------------------------
+	// 构建行号表
 
-	// section headers
-	//tw = tabwriter.NewWriter(os.Stdout, 0, 4, 3, ' ', 0)
-	//heading := "No.\tName\tType\tFlags\tAddr\tOffset\tSize\tLink\tInfo\tAddrAlign\tEntSize\tFileSize\n"
-	//fmt.Fprintf(tw, heading)
-	//for idx, s := range file.Sections {
-	//	fmt.Fprintf(tw, "%d\t%s\t%s\t%s\t%#x\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
-	//		idx, s.Name, s.Type.String(), s.Flags.String(), s.Addr, s.Offset,
-	//		s.Size, s.Link, s.Info, s.Addralign, s.Entsize, s.FileSize)
-	//}
-	//tw.Flush()
-	//println()
-
-	// .text section
-	//dat, err := file.Section(".text").Data()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Printf("% x\n", dat[:32])
-	//
-	//offset := 0
-	//for i := 0; i < 10; i++ {
-	//	inst, err := x86asm.Decode(dat[offset:], 64)
-	//	if err != nil {
-	//		break
-	//	}
-	//	fmt.Println(x86asm.GNUSyntax(inst, 0, nil))
-	//	offset += inst.Len
-	//}
-
-	//dataSection := file.Section(".data")
-	//dat, err := dataSection.Data()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//_ = dat
-	//fmt.Printf("% x\n", dat[:32])
+	fmt.Println("constructing lineno table")
 
 	gosymtab, _ := file.Section(".gosymtab").Data()
 	gopclntab, _ := file.Section(".gopclntab").Data()
 
 	pclntab := gosym.NewLineTable(gopclntab, file.Section(".text").Addr)
 	table, _ := gosym.NewTable(gosymtab, pclntab)
-
-	pc, fn, err := table.LineToPC("/root/debugger101/testdata/loop2.go", 3)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("pc => %#x\tfn => %s\n", pc, fn.Name)
-	}
-	pc, fn, _ = table.LineToPC("/root/debugger101/testdata/loop2.go", 9)
-	fmt.Printf("pc => %#x\tfn => %s\n", pc, fn.Name)
-	pc, fn, _ = table.LineToPC("/root/debugger101/testdata/loop2.go", 11)
-	fmt.Printf("pc => %#x\tfn => %s\n", pc, fn.Name)
-	pc, fn, _ = table.LineToPC("/root/debugger101/testdata/loop2.go", 17)
-	fmt.Printf("pc => %#x\tfn => %s\n", pc, fn.Name)
-
-	f, l, fn := table.PCToLine(0x4b86cf)
-	fmt.Printf("pc => %#x\tfn => %s\tpos => %s:%d\n", 0x4b86cf, fn.Name, f, l)
 	println()
 
+	// 利用行号表
+	fmt.Println("using lineno table: file:lineno to vaddr")
+	fmt.Println(strings.Repeat("-", 120))
+
+	type arg struct {
+		file   string
+		lineno int
+		pc     uintptr
+	}
+
+	args := []arg{
+		{file: "/root/debugger101/testdata/loop2.go", lineno: 3},
+		{file: "/root/debugger101/testdata/loop2.go", lineno: 9},
+		{file: "/root/debugger101/testdata/loop2.go", lineno: 11},
+		{file: "/root/debugger101/testdata/loop2.go", lineno: 17},
+	}
+
+	for _, arg := range args {
+		pc, fn, err := table.LineToPC(arg.file, arg.lineno)
+		if err != nil {
+			fmt.Printf("file:lineno to vaddr, file:lineno = %s:%d, error: %v\n", arg.file, arg.lineno, err)
+			continue
+		}
+		fmt.Printf("file:lineno to vaddr, file:lineno = %s:%d, pc = %#x, fn = %s\n", arg.file, arg.lineno, pc, fn.Name)
+	}
+	println()
+
+	fmt.Println("using lineno table: vaddr to file:lineno")
+	fmt.Println(strings.Repeat("-", 120))
+
+	f, l, fn := table.PCToLine(0x4b86cf)
+	fmt.Printf("vaddr to file:lineno, pc = %#x, fn = %s, file:lineno = %s:%d\n", 0x4b86cf, fn.Name, f, l)
+	println()
+
+	fmt.Println("using pclntab: print stacktrace")
+	fmt.Println(strings.Repeat("-", 120))
+
 	printStackTrace(table, 0x4b86cf, 20)
-
-	// dwarf调试信息遍历
-	//dw, err := file.DWARF()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//rd := dw.Reader()
-	//
-	//// next compilation unit
-	//for {
-	//	entry, err := rd.Next()
-	//	if err == io.EOF {
-	//		fmt.Println(err)
-	//		break
-	//	}
-	//	if entry == nil {
-	//		break
-	//	}
-	//
-	//	if entry.Tag == dwarf.TagCompileUnit {
-	//		fmt.Println("CompilationUnit:", entry.Field)
-	//	}
-	//
-	//	if entry.Tag == dwarf.TagSubprogram {
-	//		// 读取.debug_line关联的行号表信息
-	//		lrd, err := dw.LineReader(entry)
-	//		if err != nil {
-	//			fmt.Println(err)
-	//			break
-	//		}
-	//		if lrd == nil {
-	//			continue
-	//		}
-	//		for {
-	//			lentry := dwarf.LineEntry{}
-	//			err = lrd.Next(&lentry)
-	//			if err == io.EOF {
-	//				break
-	//			}
-	//			fmt.Printf("lineTable: %s:%d\n", lentry.File.Name, lentry.Line)
-	//		}
-	//	}
-	//
-	//	fmt.Println(entry)
-	//}
-
 }
 
 func main2() {
