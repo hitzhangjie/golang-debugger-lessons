6927059ece5790096e26f75244641c5f1d0fba6e
hitzhangjie <hit.zhangjie@gmail.com>
2020-12-04 21:04:27 +0800

    feature：dwarf相关操作

--- 7-dwarf/main.go
+++ 7-dwarf/main.go
@@ -1,1 +1,143 @@
-package __dwarf
+package main
+
+import (
+	"debug/dwarf"
+	"debug/elf"
+	"debug/gosym"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/davecgh/go-spew/spew"
+	"golang.org/x/arch/x86/x86asm"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "usage: go run main.go <prog>")
+		os.Exit(1)
+	}
+	prog := os.Args[1]
+
+	// open elf
+	file, err := elf.Open(prog)
+	if err != nil {
+		panic(err)
+	}
+	//spew.Dump(file)
+
+	// dwarf调试信息遍历
+	dw, err := file.DWARF()
+	if err != nil {
+		panic(err)
+	}
+
+	rd := dw.Reader()
+
+	// next compilation unit
+	for {
+		entry, err := rd.Next()
+		if err == io.EOF {
+			fmt.Println(err)
+			break
+		}
+		if entry == nil {
+			break
+		}
+
+		if entry.Tag == dwarf.TagCompileUnit {
+			fmt.Println("CompilationUnit:", entry.Field)
+		}
+
+		if entry.Tag == dwarf.TagSubprogram {
+			// 读取.debug_line关联的行号表信息
+			lrd, err := dw.LineReader(entry)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			if lrd == nil {
+				continue
+			}
+			for {
+				lentry := dwarf.LineEntry{}
+				err = lrd.Next(&lentry)
+				if err == io.EOF {
+					break
+				}
+				fmt.Printf("lineTable: %s:%d\n", lentry.File.Name, lentry.Line)
+			}
+		}
+
+		fmt.Println(entry)
+	}
+}
+
+func main2() {
+	wd, _ := os.Getwd()
+	file, err := elf.Open(filepath.Join(wd, "testdata/loop2"))
+	if err != nil {
+		panic(err)
+	}
+	spew.Printf("elf open ok, file:\n")
+	spew.Dump(file)
+
+	os.Exit(1)
+	// 查看section列表
+	for idx, sec := range file.Sections {
+		buf, _ := sec.Data()
+		fmt.Printf("[%d] %-16s %-16s size:%d\n", idx, sec.Name, sec.Type.String(), len(buf))
+	}
+	fmt.Println()
+
+	// 查看指令部分
+	text, _ := file.Section(".text").Data()
+	i := 0
+	count := 0
+	for {
+		if count > 10 {
+			break
+		}
+		inst, err := x86asm.Decode(text[i:], 64)
+		if err != nil {
+			break
+		}
+		fmt.Printf("%#x %-16x %s\n", i, text[i:i+inst.Len], inst.String())
+		i += inst.Len
+		count++
+	}
+	fmt.Println()
+
+	// 查看符号表部分
+	symtab, _ := file.Section(".gosymtab").Data()
+	fmt.Println("symtab size:", len(symtab))
+
+	pclntab, _ := file.Section(".gopclntab").Data()
+	linetab := gosym.NewLineTable(pclntab, file.Entry)
+
+	table, err := gosym.NewTable(symtab, linetab)
+	if err != nil {
+		panic(err)
+	}
+	for k, _ := range table.Files {
+		fmt.Println("file:", k)
+	}
+
+	for _, f := range table.Funcs {
+		fmt.Printf("func: %s\n", f.Name)
+	}
+	//fmt.Println("objs size:", len(table.Objs))
+
+	//for _, sym := range table.Syms {
+	//	fmt.Println("sym: %s\n", sym.Name)
+	//}
+	fmt.Println()
+
+	fmt.Println("main.main entry:", table.LookupFunc("main.main").Entry)
+	pc, fn, err := table.LineToPC("/root/main.go", 15)
+	fmt.Println(pc, fn.Name, err)
+
+	f, l, fn := table.PCToLine(pc)
+	fmt.Println(f, l, fn.Name)
+}
